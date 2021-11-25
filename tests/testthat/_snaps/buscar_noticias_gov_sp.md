53404ed29698623f7779ebde451b3aceaff9cb8c
Beatriz Milz <beatriz.milz@hotmail.com>
2021-11-25 15:48:26 -0300

    adiciona mais testes

---
+++ tests/testthat/_snaps/buscar_noticias_gov_sp.md
@@ -0,0 +1,10 @@
+# raspar_pagina_gov_sp works
+
+    Code
+      base_noticias_gov_sp[base_noticias_gov_sp$id == "5434704", "chamada"]
+    Output
+      # A tibble: 1 x 1
+        chamada                                                                       
+        <chr>                                                                         
+      1 "Mostra \"Artemisa: Arte Urbana Por um Outubro Além do Rosa\" está instalada ~
+
--- tests/testthat/test-buscar_noticias_gov_sp.R
+++ tests/testthat/test-buscar_noticias_gov_sp.R
@@ -14,4 +14,22 @@ test_that("raspar_pagina_gov_sp works", {
   # testando paginas sem noticias
   noticias_pagina_10000 <- raspar_pagina_gov_sp(10000)
   testthat::expect_null(noticias_pagina_10000)
+
+
+  # Problema com as aspas repetindo
+  base_noticias_gov_sp <-
+    readr::read_delim(
+      "https://raw.githubusercontent.com/beatrizmilz/noticiasgov/master/inst/base_noticias_gov_sp.csv",
+      delim = ";",
+      escape_double = TRUE,
+      col_types = readr::cols(data = readr::col_date(format = "%Y-%m-%d")),
+      trim_ws = TRUE
+    ) %>%
+    dplyr::mutate(id = as.character(id))
+
+  testthat::expect_snapshot(base_noticias_gov_sp[base_noticias_gov_sp$id == "5434704", "chamada"])
+
+  testthat::expect_gte(nrow(base_noticias_gov_sp), 92900)
+  testthat::expect_s3_class(base_noticias_gov_sp, "tbl_df")
+
 })
