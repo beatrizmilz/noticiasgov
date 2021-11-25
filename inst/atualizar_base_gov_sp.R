525223e7780b0af10bc387b8a828006873d88537
Beatriz Milz <beatriz.milz@hotmail.com>
2021-11-25 11:51:51 -0300

    remove waldo

--- inst/atualizar_base_gov_sp.R
+++ inst/atualizar_base_gov_sp.R
@@ -16,27 +16,26 @@ base_noticias_gov_sp[base_noticias_gov_sp$id == "5434704", "chamada"]
 
 # atualizar a base de dados
 base_noticias_gov_sp_atualizada <-
-  atualizar_dados_gov_sp(pag_inicial = 1, pag_final = 2) %>%
-  remover_aspas_duplicadas()# %>%
-  # remover_aspas_duplicadas() %>%
-  # remover_aspas_duplicadas() %>%
-  # remover_aspas_duplicadas() %>%
-  # remover_aspas_duplicadas() %>%
-  #remover_aspas_duplicadas()
+  atualizar_dados_gov_sp(pag_inicial = 1, pag_final = 1) %>%
+  remover_aspas_duplicadas()
 
 # verificar como ta
 base_noticias_gov_sp_atualizada[base_noticias_gov_sp_atualizada$id == "5434704", "chamada"]
 
 
-# Comparar com Waldo para escrever a mensagem de commit
-waldo_diferenca <-
-  waldo::compare(base_noticias_gov_sp[1:500,], base_noticias_gov_sp_atualizada[1:500,])
-
-frase_waldo <-
-  glue::glue("O Waldo encontrou {length(waldo_diferenca)} diferenças!") %>% as.character()
+# Comparar para escrever a mensagem de commit
+linhas_diferenca <-
+  dplyr::anti_join(base_noticias_gov_sp_atualizada,
+                   base_noticias_gov_sp) %>% nrow()
 
 commit_message <-
-  paste0("Atualiza noticias [GitHub Actions]. ", frase_waldo)
+  paste0(
+    "[GitHub Actions] Atualizando noticias. ",
+    linhas_diferenca,
+    " novas linhas adicionadas na base."
+  )
+
+commit_message
 
 writeLines(commit_message, "mensagem-comit.txt")
 
